47dd2b678b373057273cff68b05006025cd8754d
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-11-03 17:08:05 +0500

    -1 moved

--- e5/main.rb
+++ e5/main.rb
@@ -13,6 +13,6 @@ users_age = gets.chomp
 
 puts "Hello, you are " + users_age + " years old!"
 
-users_age = users_age.to_i
+users_age = users_age.to_i - 1
 puts "Hello, you are now 1 year younger."
-puts "You are now " + (users_age - 1) + " years old!"
+puts "You are now " + users_age + " years old!"
