a2936c0d4e5af964dd87fa73c9a1b46138d21a52
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-11-03 17:04:37 +0500

    attempt do decrease user's age

--- e5/main.rb
+++ e5/main.rb
@@ -14,4 +14,4 @@ users_age = gets.chomp
 puts "Hello, you are " + users_age + " years old!"
 
 puts "Hello, you are now 1 year younger."
-puts "You are now " + users_age + " years old!"
+puts "You are now " + (users_age - 1) + " years old!"
