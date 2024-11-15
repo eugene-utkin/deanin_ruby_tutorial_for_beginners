02f5d912b6dd69b0528677318343a8d8746949cc
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-11-15 19:30:17 +0500

    chomp used

--- e10/main.rb
+++ e10/main.rb
@@ -2,4 +2,4 @@ puts "Hello, I am temperature converting AI™."
 puts "Is this temperature in Fahrenheit or Celsius?"
 unit_of_measure = gets.chomp.downcase
 puts "What is the temperature?"
-temperature = gets
+temperature = gets.chomp
