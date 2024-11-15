4a4b45fe4d2e699d65eb4c47abe2d06b0c8c4cee
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-11-15 20:17:10 +0500

    comment added

--- e10/main.rb
+++ e10/main.rb
@@ -1,5 +1,9 @@
 puts "Hello, I am temperature converting AI™."
+
 puts "Is this temperature in Fahrenheit or Celsius?"
 unit_of_measure = gets.chomp.downcase
+
+# If the unit is not supported, yell at the user and exit.
+
 puts "What is the temperature?"
 temperature = gets.chomp.to_f
