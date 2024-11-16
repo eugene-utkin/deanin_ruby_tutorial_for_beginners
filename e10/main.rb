7d773d8ac95831841b8e228aa18397b4c5d14f00
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-11-16 18:16:59 +0500

    exit used

--- e10/main.rb
+++ e10/main.rb
@@ -6,6 +6,7 @@ unit_of_measure = gets.chomp.downcase
 # If the unit is not supported, yell at the user and exit.
 if unit_of_measure != "f" && unit_of_measure != "c"
   puts "You did not enter a supported unit of measurement."
+  exit
 end
 
 puts "What is the temperature?"
