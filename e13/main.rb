d4197085422cd0647944405312d9203ea6b3109b
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-11-26 20:43:24 +0500

    puts modified

--- e13/main.rb
+++ e13/main.rb
@@ -1,4 +1,4 @@
 puts "What is your weight?"
 # We use to_f because weight may contain a decimal number
 weight = gets.chomp.to_f
-puts "And what unit is this weight in?"
+puts "Is that in (L)bs or (K)g?"
