d5bbf387724995fc317c027cf27eec4d8b2a63bf
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-11-26 20:44:42 +0500

    downcase used

--- e13/main.rb
+++ e13/main.rb
@@ -2,4 +2,4 @@ puts "What is your weight?"
 # We use to_f because weight may contain a decimal number
 weight = gets.chomp.to_f
 puts "Is that in (L)bs or (K)g?"
-unit = gets.chomp
+unit = gets.chomp.downcase
