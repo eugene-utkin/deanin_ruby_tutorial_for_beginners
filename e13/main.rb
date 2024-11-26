0060b335f20546156f10332dd3d365c31c42091f
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-11-26 20:46:33 +0500

    if block created

--- e13/main.rb
+++ e13/main.rb
@@ -1,5 +1,10 @@
 puts "What is your weight?"
 # We use to_f because weight may contain a decimal number
 weight = gets.chomp.to_f
+
 puts "Is that in (L)bs or (K)g?"
 unit = gets.chomp.downcase
+
+if unit == "l"
+
+end
