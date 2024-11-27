740f5bd98cb2f9ac81e492dda46a3c2db7ba549b
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-11-27 20:41:36 +0500

    convert weight logic moved into method

--- e13/main.rb
+++ e13/main.rb
@@ -7,15 +7,13 @@ unit = gets.chomp.downcase
 
 # Let's create a function to convert the weight
 def convert_weight(weight, unit)
-
-end
-
-if unit == "l"
-  new_weight = weight / 2.205
-  puts "Your weight is #{new_weight} kg"
-elsif unit == "k"
-  new_weight = weight * 2.205
-  puts "Your weight is #{new_weight} lbs"
-else
-  puts "I don't support that unit of measurement"
+  if unit == "l"
+    new_weight = weight / 2.205
+    puts "Your weight is #{new_weight} kg"
+  elsif unit == "k"
+    new_weight = weight * 2.205
+    puts "Your weight is #{new_weight} lbs"
+  else
+    puts "I don't support that unit of measurement"
+  end
 end
