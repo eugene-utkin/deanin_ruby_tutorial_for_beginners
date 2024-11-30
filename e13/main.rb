a9f7ad2b29582f40b49b9e02ed6814013c7f9e8b
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-11-30 15:33:08 +0500

    user input for unit moved into method

--- e13/main.rb
+++ e13/main.rb
@@ -33,14 +33,12 @@ def get_user_input()
   puts "What is your weight?"
   # We use to_f because weight may contain a decimal number
   weight = gets.chomp.to_f
+
+  puts "Is that in (L)bs or (K)g?"
+  unit = gets.chomp.downcase
 end
 
 # Our logical flow
-
-
-puts "Is that in (L)bs or (K)g?"
-unit = gets.chomp.downcase
-
 convert_weight(weight, unit)
 
 
