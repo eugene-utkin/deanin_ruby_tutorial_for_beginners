b87bbdc85467fc24b419f002fcd5ef4673066990
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-12-01 17:16:22 +0500

    logic moved into method

--- e13/main.rb
+++ e13/main.rb
@@ -51,16 +51,16 @@ def print_weight_convertion(weight, unit)
 end
 
 def main()
+  weight, unit = get_user_input()
 
+  convert_weight(weight, unit)
+
+
+  puts "============================================="
+
+
+  print_weight_convertion(weight, unit)
 end
 
 # Our logical flow
-weight, unit = get_user_input()
 
-convert_weight(weight, unit)
-
-
-puts "============================================="
-
-
-print_weight_convertion(weight, unit)
