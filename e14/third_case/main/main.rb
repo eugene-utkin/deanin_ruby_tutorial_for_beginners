88e900b0b2f3907063345048137ad255cb4f5cae
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-12-03 22:09:23 +0500

    main folder created

---
+++ e14/third_case/main/main.rb
@@ -0,0 +1,16 @@
+# We want to bring in the weight.rb file.
+# More specifically, we need the get_user_input()
+# and print_weight_convertion() functions.
+# We can do this by using the require_relative keyword.
+require_relative "../weight"
+
+def main()
+  weight, unit = get_user_input()
+
+  convert_weight(weight, unit)
+  puts "============================================="
+  print_weight_convertion(weight, unit)
+end
+
+# Our logical flow
+main()
