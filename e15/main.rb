e9dcb01c336ac5f3a9011baf0e705b551abd1a47
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-12-12 17:17:18 +0500

    puts used instead of print

--- e15/main.rb
+++ e15/main.rb
@@ -12,51 +12,53 @@
 # A user can quit by typing Q or q.
 
 def main()
-  puts "Welcome to the calculator"
-  puts "What would you like to do?"
-  puts "1. Addition"
-  puts "2. Subtraction"
-  puts "3. Multiplication"
-  puts "4. Division"
-  puts "Q. Quit"
-  command = gets.chomp.downcase
+  while true
+    puts "Welcome to the calculator"
+    puts "What would you like to do?"
+    puts "1. Addition"
+    puts "2. Subtraction"
+    puts "3. Multiplication"
+    puts "4. Division"
+    puts "Q. Quit"
+    command = gets.chomp.downcase
 
-  if command == "q"
-    puts "Goodbye!"
-    return
-  end
+    if command == "q"
+      puts "Goodbye!"
+      return
+    end
 
-  if command == "1"
-    puts "You chose addition"
-  elsif command == "2"
-    puts "You chose subtraction"
-  elsif command == "3"
-    puts "You chose multiplication"
-  elsif command == "4"
-    puts "You chose division"
-  else
-    puts "I don't understand that command"
-    return
-  end
+    if command == "1"
+      puts "You chose addition"
+    elsif command == "2"
+      puts "You chose subtraction"
+    elsif command == "3"
+      puts "You chose multiplication"
+    elsif command == "4"
+      puts "You chose division"
+    else
+      puts "I don't understand that command"
+      return
+    end
 
-  puts "What is the first number?"
-  first_number = gets.chomp.to_f
+    puts "What is the first number?"
+    first_number = gets.chomp.to_f
 
-  puts "What is the second number?"
-  second_number = gets.chomp.to_f
+    puts "What is the second number?"
+    second_number = gets.chomp.to_f
 
-  if command == "1"
-    result = first_number + second_number
-    print(result)
-  elsif command == "2"
-    result = first_number - second_number
-    print(result)
-  elsif command == "3"
-    result = first_number * second_number
-    print(result)
-  elsif command == "4"
-    result = first_number / second_number
-    print(result)
+    if command == "1"
+      result = first_number + second_number
+      puts(result)
+    elsif command == "2"
+      result = first_number - second_number
+      print(result)
+    elsif command == "3"
+      result = first_number * second_number
+      print(result)
+    elsif command == "4"
+      result = first_number / second_number
+      print(result)
+    end
   end
 end
 
