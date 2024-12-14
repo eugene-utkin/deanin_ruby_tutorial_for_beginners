63cf7bd3aa816b6ed4553da8f44a279400ff2b05
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-12-14 14:57:00 +0500

    command_is_invalid variable created

--- e15/main.rb
+++ e15/main.rb
@@ -39,6 +39,7 @@ end
 
 def invalid_command(command)
   command_does_not_equal_number = command != "1" && command != "2" && command != "3" && command != "4"
+  command_is_invalid = command_does_not_equal_number && command != "q"
   if command_does_not_equal_number && command != "q"
     puts "I don't understand that command"
     return true
