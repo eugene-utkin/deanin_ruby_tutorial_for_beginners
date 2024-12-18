48e85f82d1a2b5e6b2d57a221be29fd04666c241
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-12-18 18:58:53 +0500

    division method moved into operations

--- e15/math/operations.rb
+++ e15/math/operations.rb
@@ -12,3 +12,12 @@ def multiply(first_number, second_number)
   result = first_number * second_number
   puts(result)
 end
+
+def divide(first_number, second_number)
+  if second_number == 0
+    puts "You can't divide by zero"
+  else
+    result = first_number / second_number
+    puts(result)
+  end
+end
