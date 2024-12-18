b1af2f2ae7664cc53f4b803310372eb925a03070
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-12-18 21:47:04 +0500

    main.rb updated

--- e15/main.rb
+++ e15/main.rb
@@ -23,15 +23,6 @@ def print_welcome_message()
   puts "Q. Quit"
 end
 
-def invalid_command(command)
-  command_does_not_equal_number = command != "1" && command != "2" && command != "3" && command != "4"
-  command_is_invalid = command_does_not_equal_number && command != "q"
-  if command_is_invalid
-    puts "I don't understand that command"
-    return true
-  end
-end
-
 def check_for_quit(command)
   if command == "q"
     puts "Goodbye!"
