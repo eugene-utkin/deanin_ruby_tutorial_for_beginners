4aca4bbb2cd79456d96ae98bef5db94f705f99f8
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-12-18 19:08:02 +0500

    get_user_numbers method moved

---
+++ e15/input/user_input.rb
@@ -0,0 +1,9 @@
+def get_user_numbers()
+  puts "What is the first number?"
+  first_number = gets.chomp.to_f
+
+  puts "What is the second number?"
+  second_number = gets.chomp.to_f
+
+  return first_number, second_number
+end
