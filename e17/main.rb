b3f9261bc858a676fda6be742f62c2fea5cdf265
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-12-23 19:29:22 +0500

    first argument added

--- e17/main.rb
+++ e17/main.rb
@@ -3,7 +3,7 @@ require_relative 'player'
 
 # Create a new player
 player = Player.new(100, 50, 1)
-dean = Player.new()
+dean = Player.new(120)
 
 # print the player's health
 player.print_my_health()
