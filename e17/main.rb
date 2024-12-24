e8f663fc7f38aba8df5100b3bc4078af5895b80f
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-12-24 20:43:54 +0500

    print_my_name called

--- e17/main.rb
+++ e17/main.rb
@@ -6,6 +6,7 @@ player = Player.new("John Doe", 100, 50, 1)
 dean = Player.new("Dean", 120, 60, 2)
 
 # print the player's health
+player.print_my_name
 player.print_my_health()
 
 # print Dean's health
