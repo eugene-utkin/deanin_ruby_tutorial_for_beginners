90f8e0d5b50e601056c5c7472d8967e1ab4a0003
Evgeniy Utkin <eugene.utkin@gmail.com>
2024-12-24 20:42:06 +0500

    name added for player

--- e17/main.rb
+++ e17/main.rb
@@ -2,7 +2,7 @@
 require_relative 'player'
 
 # Create a new player
-player = Player.new(100, 50, 1)
+player = Player.new("John Doe", 100, 50, 1)
 dean = Player.new(120, 60, 2)
 
 # print the player's health
